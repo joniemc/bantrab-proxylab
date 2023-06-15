9c5f8e616f2551279d6b21e6e38d24a0082a6aad
jonie_mirc <jonie.mirc@gmail.com>
2023-06-15 11:21:38 -0600

    Update first project

---
+++ proxylab/src/main/java/com/bantrab/proxylab/ProxylabApplication.java
@@ -0,0 +1,13 @@
+package com.bantrab.proxylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxylabApplication.class, args);
+	}
+
+}
---
+++ proxylab/src/test/java/com/bantrab/proxylab/ProxylabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantrab.proxylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
